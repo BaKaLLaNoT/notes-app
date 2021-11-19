a84d83c8b4bd22964f447fd90f896cd9ed76408d
BaKaLLaNoT <allakabcn@gmail.com>
2021-11-19 17:39:13 +0100

    useRouteMatch don't work

--- app/src/App.js
+++ app/src/App.js
@@ -1,38 +1,85 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom'
+import React, { lazy } from 'react'
+import { BrowserRouter, Routes, Route, Link, Navigate, useRouteMatch } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 import { NoteDetail } from './components/NoteDetail'
 import { useUser } from './hooks/useUser'
 import { useNotes } from './hooks/useNotes'
-import Notes from './Notes'
 import Login from './Login'
 
-const Home = () => <h1>Home Page</h1>
+const LazyNotes = lazy(() => import('./Notes'))
+
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Home | Notes App</title>
+      </Helmet>
+      <h1>Home Page</h1>
+    </>
+  )
+}
+
 const Users = () => <h1>Users</h1>
 
 const App = () => {
   const { notes } = useNotes()
   const { user } = useUser()
+
   const inlineStyles = {
     padding: 5
   }
+  const match = useRouteMatch('/notes/:id')
   return (
     <BrowserRouter>
       <header>
-        <Link to='/' style={inlineStyles}>Home</Link>
-        <Link to='/notes' style={inlineStyles}>Notes</Link>
-        <Link to='/users' style={inlineStyles}>Users</Link>
-        {user
-          ? <em>Logged as {user.name}</em>
-          : <Link to='/login' style={inlineStyles}>Login</Link>}
+        <Link to='/' style={inlineStyles}>
+          Home
+        </Link>
+        <Link to='/notes' style={inlineStyles}>
+          Notes
+        </Link>
+        <Link to='/users' style={inlineStyles}>
+          Users
+        </Link>
+        {
+          user
+            ? <em>Logged as {user.name}</em>
+            : (
+              <Link to='/login' style={inlineStyles}>
+                Login
+              </Link>
+              )
+        }
       </header>
       <Routes>
-        <Route path='/notes/:noteId' element={<NoteDetail notes={notes} />} />
-        <Route path='/notes' element={<Notes />} />
-        <Route path='/users' element={<Users />} />
-        <Route path='/login' element={user ? <Navigate replace to='/' /> : <Login />} />
-        <Route path='/' element={<Home />} />
+        <Route
+          path='/login' render={() => {
+            return user ? <Navigate to='/' /> : <Login />
+          }}
+        />
+        <Route exact path='/notes'>
+          <LazyNotes />
+        </Route>
+        <Route
+          path='/notes/:noteId' render={() => {
+            const note = match
+              ? notes.find(note => note.id === match.params.id)
+              : null
+            return <NoteDetail note={note} />
+          }}
+        />
+        <Route path='/users'>
+          {user ? <Users /> : <Navigate to='/login' />}
+        </Route>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route>
+          <h1>Not found</h1>
+        </Route>
       </Routes>
     </BrowserRouter>
+
   )
 }
 
